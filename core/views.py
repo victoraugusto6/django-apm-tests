11b5bc2320155d1927f896b374de5d86dc24933f
Victor Augusto <victor.oliveira@onidata.com>
2023-08-11 07:49:53 -0300

    add prefetch

--- core/views.py
+++ core/views.py
@@ -4,5 +4,5 @@ from .models import Book
 
 
 def home(request):
-    books = Book.objects.all()
+    books = Book.objects.all().prefetch_related("authors")
     return render(request, "core/home.html", {"books": books})
