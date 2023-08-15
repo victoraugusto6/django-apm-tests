4204ea0b2604ee64f1fcbe0a21027fae9e30f595
Victor Augusto <victor.oliveira@onidata.com>
2023-08-15 07:32:33 -0300

    change to django.conf

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from projeto.settings import DEBUG
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
